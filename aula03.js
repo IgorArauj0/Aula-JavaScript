6064c8da52b235c7dd1f1206487a19578fdb1ba8
IgorArauj0 <igoritoaraujo223@gmail.com>
2025-05-08 21:44:48 -0300

    Salvamento

---
+++ aula03.js
@@ -0,0 +1,2 @@
+let testeOperacao = `Texto de exemplo para string template ${1 + 1}`;
+console.log(testeOperacao);
---
+++ aula04.js
@@ -0,0 +1,16 @@
+var nome = 'Igor'; //Criamos uma variável tipo String para guardar o nome do usuário
+var idade = 24; //Criamos uma variável tipo number para guardar a idade do usuário
+
+var mensagem = `Olá, meu nome é ${nome} e tenho ${idade} anos de idade`; //Criamos uma variável chamada mensagem para exibir o texto contendo as duas variáveis que foram declaradas
+console.log(mensagem); //Criamos o Console.log para fazer a impressão da variável "mensagem".
+
+
+//criem uma mensagem para informar profissão e salário de um Trabalhador
+var profissao = "professor";
+var salario = 5046
+var retornoSalario = `A profissão dele é ${profissao} e ele recebe ao mês ${salario} reais`;
+console.log(retornoSalario);
+
+
+//criem uma mensagem para informar raça e idade de um cachorro
+
